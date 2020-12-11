d77dc72d035e17212ab2103aa97482d8e37105a8
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2020-12-11 16:19:39 +0100

    feat: dynamic pages

---
+++ pages/_slug.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container">
+    <div>
+      <img :src="planet.image" :alt="planet.title" />
+      <h1 class="title">{{ planet.title }}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ params }) {
+    const planet = await fetch(
+      `https://api.nuxtjs.dev/planets/${params.slug}`
+    ).then((res) => res.json())
+    return { planet }
+  },
+}
+</script>
+
+<style>
+img {
+  height: 80px;
+  width: auto;
+  object-fit: cover;
+}
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
