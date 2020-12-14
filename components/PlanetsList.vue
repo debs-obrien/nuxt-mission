0802b2c5e285ff5a8e50594c829c2ccc7ecc11db
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2020-12-14 18:34:54 +0100

    feat: lazy loading components

---
+++ components/PlanetsList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p v-if="$fetchState.pending">Fetching planets....</p>
+    <p v-else-if="$fetchState.error">Error while fetching planets</p>
+    <ul v-else>
+      <li v-for="planet in planets" :key="planet.slug">
+        <NuxtLink :to="planet.slug">
+          {{ planet.title }}
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  async fetch() {
+    this.planets = await fetch('https://api.nuxtjs.dev/planets').then((res) =>
+      res.json()
+    )
+  },
+  data() {
+    return {
+      planets: [],
+    }
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,34 +3,24 @@
     <div>
       <Logo />
       <h1 class="title">nuxt-mission</h1>
-
-      <p v-if="$fetchState.pending">Fetching planets....</p>
-      <p v-else-if="$fetchState.error">Error while fetching planets</p>
-      <ul v-else>
-        <li>
-          <NuxtLink to="/nuxt">Nuxt</NuxtLink>
-        </li>
-        <li v-for="planet in planets" :key="planet.slug">
-          <NuxtLink :to="planet.slug">
-            {{ planet.title }}
-          </NuxtLink>
-        </li>
-      </ul>
+      <button @click="showPlanets">Show Planets</button>
+      <template v-if="show">
+        <LazyPlanetsList />
+      </template>
     </div>
   </div>
 </template>
-
 <script>
 export default {
-  async fetch() {
-    this.planets = await fetch('https://api.nuxtjs.dev/planets').then((res) =>
-      res.json()
-    )
-  },
   data() {
     return {
-      planets: [],
+      show: false,
     }
+  },
+  methods: {
+    showPlanets() {
+      this.show = true
+    },
   },
 }
 </script>
