7598e09bf475e057037ab1eb806caf127e368c8b
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2020-12-15 16:51:46 +0100

    feat: lesson 8

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -10,6 +10,7 @@
 
 <script>
 export default {
+  transition: 'bounce',
   head() {
     return {
       title: this.planet.title,
@@ -46,41 +47,9 @@ export default {
 h1 {
   font-family: Nunito, sans-serif;
 }
-</style>
-<style>
 img {
   height: 80px;
   width: auto;
   object-fit: cover;
-}
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
